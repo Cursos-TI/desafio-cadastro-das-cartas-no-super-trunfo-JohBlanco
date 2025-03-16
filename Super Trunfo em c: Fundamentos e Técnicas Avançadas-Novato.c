b55b7ae47d4322158641daa05ad657911497a8d8
JohBlanco <91393411+JohBlanco@users.noreply.github.com>
2025-03-16 14:45:03 -0300

    Update and rename CartasSuperTrunfo-Novato.c to Super Trunfo em c: Fundamentos e Técnicas Avançadas-Novato.c
    Arquivo corrigido e renomeado para avaliação.

--- Super Trunfo em c: Fundamentos e Técnicas Avançadas-Novato.c
+++ Super Trunfo em c: Fundamentos e Técnicas Avançadas-Novato.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <string.h>
+#include <string.h> 
 
 int main(){
 
@@ -19,9 +19,9 @@ int main(){
     scanf(" %c",&c_estado_1);
     printf("Insira o código da cidade: ");
     scanf(" %s", c_cidade_1);
-    snprintf(c_carta_1, 4, "%c%s", c_estado_1, c_cidade_1);
+    snprintf(c_carta_1, 4, "%c%s", c_estado_1, c_cidade_1); //Concatena as duas strings em uma variável
     printf("Insira o nome da cidade: ");
-    while (getchar() != '\n');
+    while (getchar() != '\n'); //Limpa o buffer do teclado
     fgets(n_cidade_1, 20, stdin);
     n_cidade_1[strcspn(n_cidade_1, "\n")] = 0;
     printf("Insira a população da cidade: ");
@@ -39,9 +39,9 @@ int main(){
     scanf(" %c",&c_estado_2);
     printf("Insira o código da cidade: ");
     scanf(" %s", c_cidade_2);
-    snprintf(c_carta_2, 4, "%c%s", c_estado_2, c_cidade_2);
+    snprintf(c_carta_2, 4, "%c%s", c_estado_2, c_cidade_2); //Concatena as duas strings em uma variável
     printf("Insira o nome da cidade: ");
-    while (getchar() != '\n');
+    while (getchar() != '\n'); //Limpa o buffer do teclado
     fgets(n_cidade_2, 20, stdin);
     n_cidade_2[strcspn(n_cidade_2, "\n")] = 0;
     printf("Insira a população da cidade: ");
@@ -53,12 +53,6 @@ int main(){
     printf("Insira o número de pontos turísticos: ");
     scanf(" %d", &n_turismo_2);
 
-    //Calcula Densidade Populacional e PIB per capita
-    float densidade_1 = populacao_1/area_1;
-    float densidade_2 = populacao_2/area_2;
-    float PIB_per_capita_1 = PIB_1/populacao_1;
-    float PIB_per_capita_2 = PIB_2/populacao_2;
-
     //Saída de dados da carta
     puts("\nPrimeira Carta");
     printf("\nEstado: %c",c_estado_1);
@@ -68,8 +62,6 @@ int main(){
     printf("\nArea: %.2f km²",area_1);
     printf("\nPIB: %.2f bilhoes de reais",PIB_1);
     printf("\nNumero de pontos turisticos: %d",n_turismo_1);
-    printf("\nDensidade Populacional: %.2f hab/km²",densidade_1);
-    printf("\nPIB per capita: %.2f reais/hab",PIB_per_capita_1);
 
     //Saída de dados da carta
     puts("\nSegunda Carta");
@@ -80,23 +72,6 @@ int main(){
     printf("\nArea: %.2f km²",area_2);
     printf("\nPIB: %.2f bilhoes de reais",PIB_2);
     printf("\nNumero de pontos turisticos: %d",n_turismo_2);
-    printf("\nDensidade Populacional: %.2f hab/km²",densidade_2);
-    printf("\nPIB per capita: %.2f reais/hab",PIB_per_capita_2);
-
-    //Comparação de dados
-    if (populacao_1 > populacao_2){
-        printf("\nCarta 1 - %s: %d",n_cidade_1,populacao_1);
-        printf("\nCarta 2 - %s: %d",n_cidade_2,populacao_2);
-        puts("\nA primeira carta venceu!");
-    } else if (populacao_1 < populacao_2){
-        printf("\nCarta 1 - %s: %d",n_cidade_1,populacao_1);
-        printf("\nCarta 2 - %s: %d",n_cidade_2,populacao_2);
-        puts("\nA segunda carta venceu!");
-    } else {
-        printf("\nCarta 1 - %s: %d",n_cidade_1,populacao_1);
-        printf("\nCarta 2 - %s: %d",n_cidade_2,populacao_2);
-        puts("\nAs cartas empataram!");
-    }
 
     return 0;
 }
